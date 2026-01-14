cfe127dcee4876d7ae4d23587e47cea066ea9f44
Sajal <protrudefilth29@gmail.com>
2026-01-14 18:01:17 +0545

    Update main.py
    Fixed a major bug when dealing with  '\n'. Semicolons are now optional

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 from Tokenizer import Tokenizer 
 from Interpreter import Interpreter
-
-
-
 Code = """
+|Declare an array|
 decm 200; 
-decv store int; 
-minus 10,2 store; 
-out store; 
+decf X a,b,c; 
+out a;
+out b;
+out c;
+endf;
+call X 10,20,30
 """
+Code = Code.split("\n")
+for iter1 in range(len(Code)):
+    Code[iter1] = Code[iter1].strip()
+    try:
+        if Code[iter1][-1] != ";":Code[iter1]+=";"
+    except:Code[iter1] = ";"
+Code = "\n".join(Code)
 Code = Code.split(";")
 CodeLines = []
-for each in Code:
-    each.strip("\n").rstrip()
-    if "\n" in each:
-        each = [char.strip("\n") for char in each.split()]
-        each = " ".join(each)
-    each += ';'
-    CodeLine = Tokenizer().Tokenize(each)
+for counter in range(len(Code)):
+    Code[counter].strip("\n").rstrip()
+    if "\n" in Code[counter]:
+        Code[counter] = [char.strip("\n") for char in Code[counter].split()]
+        Code[counter] = " ".join(Code[counter])
+    Code[counter] += ';'
+    CodeLine = Tokenizer().Tokenize(Code[counter])
     CodeLines.append(CodeLine)
-Interpreter(CodeLines).Interpret(0) 
+Interpreter(CodeLines).Interpret(0)
