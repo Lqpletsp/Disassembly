cf863185ee52ca09f772242f83947fae37f0204b
Sajal <protrudefilth29@gmail.com>
2026-01-17 22:49:59 +0545

    Update main.py
    Now 'disassembly' can be used as a command. Needs an executable

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-from Tokenizer import Tokenizer 
+#!/usr/bin/env python3
+import sys
+import os
+from Tokenizer import Tokenizer
 from Interpreter import Interpreter
 
-Code = """
-decm 200; 
-decf X a,b,c; 
-out a;
-out b;
-out c;
-endf;
-call X 10,20,30
-"""
-Interpreter(Tokenizer().HandleCode(Code)).Interpret(0)
+if len(sys.argv)<2:
+    print("Usage: disassembly <filename>.ds")
+    sys.exit(1)
+
+filename = sys.argv[1]
+
+if not filename.endswith(".ds"):
+    print("Error[FILEERROR]: File must end with .ds")
+    sys.exit(1)
+
+if not os.path.isfile(filename):
+    print(f"Error[FILEERROR]: '{filename}' not found")
+    sys.exit(1)
+
+pathdir = filename.split("/")
+pathdir = "/".join(pathdir[:len(pathdir)-1])
+
+with open(filename, "r") as f:
+    code = f.read()
+
+CodeLines = Tokenizer().HandleCode(code)
+Interpreter(pathdir).Interpret(0,CodeLines)
