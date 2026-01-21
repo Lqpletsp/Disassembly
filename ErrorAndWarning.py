49ca583b1a7e8d040b812d492849c9e85db645a4
Sajal <protrudefilth29@gmail.com>
2026-01-21 22:25:50 +0545

    Update ErrorAndWarning.py
    Handling negative lines and more detailed error message (<filename>@<error pointer>)

--- ErrorAndWarning.py
+++ ErrorAndWarning.py
@@ -1,6 +1,17 @@
 class Errors:
-    def OutError(self,ErrorType,ErrorPointer) -> None: 
-        print(f"ERROR[{ErrorPointer}] : {ErrorType}")   
+    def OutError(self,ErrorType,ErrorPointer) -> None:
+        from Interpreter import Interpreter
+        currentfile = Interpreter().getCurrentfile()
+        if int(ErrorPointer) < 0 and currentfile == "!/main": 
+            print("CRASH:Negative line-pointer for '!/main' metadata.")
+            print("Try changing the file-name(s) if 'bring' command is used")
+            exit()
+        elif int(ErrorPointer) < 0 and currentfile != "!/main":
+            print("CRASH:Negative line-pointer for non '!/main' metadata.")
+            print("High probability of an edge case.")
+            exit()
+
+        print(f"ERROR[{currentfile}@{ErrorPointer}] : {ErrorType}")   
         exit()
 
 
