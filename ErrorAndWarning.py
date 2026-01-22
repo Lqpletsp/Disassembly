8012d712a2cdc507622ac2b642069108178684b1
Sajal <protrudefilth29@gmail.com>
2026-01-22 09:45:33 +0545

    Update ErrorAndWarning.py
    Handling edge cases better

--- ErrorAndWarning.py
+++ ErrorAndWarning.py
@@ -2,16 +2,18 @@ class Errors:
     def OutError(self,ErrorType,ErrorPointer) -> None:
         from Interpreter import Interpreter
         currentfile = Interpreter().getCurrentfile()
-        if int(ErrorPointer) < 0 and currentfile == "!/main": 
-            print("CRASH:Negative line-pointer for '!/main' metadata.")
-            print("Try changing the file-name(s) if 'bring' command is used")
-            exit()
-        elif int(ErrorPointer) < 0 and currentfile != "!/main":
-            print("CRASH:Negative line-pointer for non '!/main' metadata.")
-            print("High probability of an edge case.")
-            exit()
-
-        print(f"ERROR[{currentfile}@{ErrorPointer}] : {ErrorType}")   
+        actuallines = Interpreter().getactuallines()
+        try:
+            if int(ErrorPointer) < 0 and currentfile == "!/main": 
+                print("CRASH:Negative line-pointer for '!/main' metadata.")
+                print("Try changing the file-name(s) if 'bring' command is used")
+                exit()
+            elif int(ErrorPointer) < 0 and currentfile != "!/main":
+                print("CRASH:Negative line-pointer for non '!/main' metadata.")
+                print("High probability of an edge case.")
+                exit()
+        except:pass
+        print(f"ERROR[{currentfile}@{actuallines}] : {ErrorType}")   
         exit()
 
 
