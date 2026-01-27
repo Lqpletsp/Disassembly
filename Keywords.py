47ff3cd115d9bd71221dc13f9ecea557d706bbd0
Sajal <protrudefilth29@gmail.com>
2026-01-27 13:21:14 +0545

    Update Keywords.py
    Removed old commands that are trivial and also removed the data type attribute

--- Keywords.py
+++ Keywords.py
@@ -1,19 +1,17 @@
 class Keyword: 
     def __init__(self) -> None:
         self.__Keywords:list = ["out","in","inc","dec", "decv","varchar","int","bool"
-                                ,"set","empt", "add", "minus","mult", "div","decf", "endf",
-                                "call","temp","decl","endl","!dne","!e","loop"]
-        self.__OneDataCommands:list = ["decm"]
+                                ,"set", "add", "minus","mult", "div","decf", "endf",
+                                "call", "temp"]
+        self.__OneDataCommands:list = ["empt","decm","else"]
         self.__TwoOrMoreDataCommands:list = ["out","inc","dec","decv","decf","set", "add",
-                                                "minus","div", "mult","cmpt","cmpf",
-                                             "decl", "in", "loop"]
-        self.__Datatypes:list = ["varchar","int","float","bool"]
-        self.__Commands:list = ["out","in","dec","decv","set","empt","add","minus","mult","div","decf",
-                                "endf","in","inc","call","decm","cmpt","decl","cmpf","endl",
-                                "loop"]
+                                                "minus","div", "mult", "parm","cmpt","cmpf",
+                                             "decl", "in", "loop","mkcmd"]
+        self.__Commands:list = ["out","in","dec","decv","set","add","minus","mult","div","decf",
+                                "endf","parm","in","inc","call","decm","cmpt","decl","cmpf","endl",
+                                "loop","mkcmd","bring","else"]
 
     def GetKeywords(self) -> list:return self.__Keywords
     def GetOneVariableCommand(self) -> list: return self.__OneDataCommands
     def GetTwoOrMoreVariableCommand(self) -> list: return self.__TwoOrMoreDataCommands
-    def GetDataTypes(self) -> list: return self.__Datatypes
     def GetCommands(self)-> list: return self.__Commands
