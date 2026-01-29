f5ad16fe24971a10f572152d198781599c08be88
Sajal <protrudefilth29@gmail.com>
2026-01-29 13:02:14 +0545

    Update ErrorAndWarning.py

--- ErrorAndWarning.py
+++ ErrorAndWarning.py
@@ -5,7 +5,7 @@ class Errors:
             if int(ErrorPointer) < 0 and currentfile == "!/main": 
                 print("CRASH:Negative line-pointer for '!/main' metadata.")
                 print("High probability of an edge case.")
-                exit()
+                sys.exit(1)
             elif int(ErrorPointer) < 0 and currentfile != "!/main":
                 print("CRASH:Negative line-pointer for non '!/main' metadata.")
                 print("High probability of an edge case.")
